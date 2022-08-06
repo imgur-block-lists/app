9e13dd2c6c923bbc89f927f0471fc42900d6e357
George Dietrich <george@dietrich.app>
2022-08-06 01:00:39 -0400

    Slim things down to 1 route and 1 component

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
